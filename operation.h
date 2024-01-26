8dc1e8b5b09a98193a0d46bf59e4605f526b62b3
Lucas Aubard <lucas.aubard@algosup.com>
2024-01-26 10:10:50 +0100

    Update operation.h
    I added the SUB, DIV and MUL operation.

--- operation.h
+++ operation.h
@@ -5,8 +5,8 @@
 #include <stdio.h>
 #include "gate_operation.h"
 
-uint16_t instruct = 15 ;
-uint16_t arm_regs [16];
+uint16_t instruct = 15;
+uint16_t arm_regs[16];
 
 enum operation
 {
@@ -16,75 +16,72 @@ enum operation
     OP_mul,
 };
 
-// enum registers
-// {
-//     R1,
-//     R2,
-//     R3,
-//     R4,
-//     R5,
-//     R6,
-//     R7,
-//     R8,
-//     R9,
-//     R10,
-//     R11,
-//     IP, // (also known as Intra-Procedure-call scratch register)
-//     SP, // (also known as Stack Pointer)
-//     LR, // (also known as Link Register)
-//     PC, // (also known as Program Counter)
-// };
-
 void ADD();
 void SUB();
 void DIV();
 void MUL();
 
-
 uint16_t sign_extend(uint16_t x, int bit_count)
 {
-    if ((x >> (bit_count - 1)) & 1) {
+    if ((x >> (bit_count - 1)) & 1)
+    {
         x |= (0xFFFF << bit_count);
     }
     return x;
 }
+
 void ADD()
 {
-
     uint16_t R1 = (instruct >> 9) & 0x7; // destination address
-
     uint16_t R2 = (instruct >> 7) & 0x7; // first register
-
     uint16_t R3 = (instruct >> 4) & 0x7; // second register
-
     uint16_t imm_val = (instruct >> 5) & 0x1; //whether we have an immediate mode or not
 
-    if(imm_val)
+    if (imm_val)
     {
-         uint16_t imm5 = sign_extend(instruct & 0x1F, 5);
-         arm_regs[R1] = arm_regs[R2] + imm5;
+        uint16_t imm5 = sign_extend(instruct & 0x1F, 5);
+        arm_regs[R1] = arm_regs[R2] + imm5;
     }
     else
     {
-        arm_regs[R1] = arm_regs[R2] + arm_regs[3];
+        arm_regs[R1] = arm_regs[R2] + arm_regs[R3];
     }
 }
 
 void SUB()
 {
+    uint16_t R1 = (instruct >> 9) & 0x7; // destination address
+    uint16_t R2 = (instruct >> 7) & 0x7; // first register
+    uint16_t R3 = (instruct >> 4) & 0x7; // second register
+    uint16_t imm_val = (instruct >> 5) & 0x1; //whether we have an immediate mode or not
 
+    if (imm_val)
+    {
+        uint16_t imm5 = sign_extend(instruct & 0x1F, 5);
+        arm_regs[R1] = arm_regs[R2] - imm5;
+    }
+    else
+    {
+        arm_regs[R1] = arm_regs[R2] - arm_regs[R3];
+    }
 }
 
 void DIV()
 {
+    uint16_t R1 = (instruct >> 9) & 0x7; // destination address
+    uint16_t R2 = (instruct >> 7) & 0x7; // first register
+    uint16_t R3 = (instruct >> 4) & 0x7; // second register
 
+    arm_regs[R1] = arm_regs[R2] / arm_regs[R3];
 }
 
 void MUL()
 {
+    uint16_t R1 = (instruct >> 9) & 0x7; // destination address
+    uint16_t R2 = (instruct >> 7) & 0x7; // first register
+    uint16_t R3 = (instruct >> 4) & 0x7; // second register
 
+    arm_regs[R1] = arm_regs[R2] * arm_regs[R3];
 }
-
-
 
 #endif
