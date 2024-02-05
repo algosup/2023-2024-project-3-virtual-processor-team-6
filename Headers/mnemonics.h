f87440d4d026d6153b58243d65f4cf99af748ec4
MichelRiff <michel.riff@algosup.com>
2024-02-05 16:47:54 +0100

    CMP, JMP, JE and RET instructions
    added more instructions

--- Headers/mnemonics.h
+++ Headers/mnemonics.h
@@ -19,18 +19,68 @@ enum mnemonics
 
 int flags; // Declare the flags variable
 uint16_t MOV(uint16_t registerIndex, uint16_t memoryLocation);
-void LOAD(int registerIndex, int memoryLocation);
-void STORE(int registerIndex, int memoryLocation);
-void CMP(int registerIndex1, int registerIndex2);
-void JMP(int address);
-void JE(int address);
-void CALL(int address);
-void RET();
+uint16_t LOAD(int registerIndex, int memoryLocation);
+uint16_t STORE(int registerIndex, int memoryLocation);
+uint16_t CMP(int registerIndex1, int registerIndex2);
+uint16_t JMP(int address);
+uint16_t JE(int address);
+uint16_t CALL(int address);
+uint16_t RET();
 
 
 uint16_t MOV(uint16_t memoryLocation, uint16_t registerIndex)
 {
    return memoryLocation = registerIndex;
 }
+
+uint16_t LOAD(int registerIndex, int memoryLocation)
+{
+
+}
+
+uint16_t STORE(int registerIndex, int memoryLocation)
+{
+
+}
+
+uint16_t CMP(int registerIndex1, int registerIndex2)
+{
+    if (registerIndex1 == registerIndex2)
+    {
+        flags = 1;
+    }
+    else
+    {
+        flags = 0;
+    }
+}
+
+uint16_t JMP(int address)
+{
+    return address;
+}
+
+uint16_t JE(int address)
+{
+    if (flags == 1)
+    {
+        return address;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+uint16_t CALL(int address)
+{
+    
+}
+
+uint16_t RET()
+{
+    return 0;
+}
+
 
 #endif
