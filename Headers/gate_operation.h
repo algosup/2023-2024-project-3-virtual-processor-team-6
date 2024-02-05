02f5dbe70054828d57c03f29d43c7ed9faad0171
Lucas Aubard <lucas.aubard@algosup.com>
2024-02-05 16:25:25 +0100

    Update gate_operation.h

--- Headers/gate_operation.h
+++ Headers/gate_operation.h
@@ -1,18 +1,13 @@
 #ifndef GATE_OPERATION_H
 #define GATE_OPERATION_H
-
-#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
-
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX];
-
 uint16_t mem_read(uint16_t address)
 {
     return memory[address];
 }
-
 enum registers
 {
     R1 = 0,
@@ -31,35 +26,23 @@ enum registers
     R_LR, // (also known as Link Register)
     R_PC, // (also known as Program Counter)
 };
-
 uint16_t arm_regs[16]; // Increase the size to 16 to accommodate all registers
-
-void initialize_arm_regs() 
-{
+void initialize_arm_regs() {
     enum {
         PC_START = 0x3000,
     };
     arm_regs[R_PC] = PC_START;
 }
-
-void XOR(uint16_t reg1, uint16_t reg2)
-{
-    arm_regs[reg1] ^= arm_regs[reg2];
+uint16_t AND(uint16_t op1, uint16_t op2) {
+    return op1 & op2;
 }
-
-void AND(uint16_t reg1, uint16_t reg2)
-{
-    arm_regs[reg1] &= arm_regs[reg2];
+uint16_t OR(uint16_t op1, uint16_t op2) {
+    return op1 | op2;
 }
-
-void OR(uint16_t reg1, uint16_t reg2)
-{
-    arm_regs[reg1] |= arm_regs[reg2];
+uint16_t XOR(uint16_t op1, uint16_t op2) {
+    return op1 ^ op2;
 }
-
-void NOT(uint16_t reg1)
-{
-    arm_regs[reg1] = ~arm_regs[reg1];
+uint16_t NOT(uint16_t op1) {
+    return ~op1;
 }
-
 #endif
