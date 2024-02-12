e4d3cdfac4522d0329297bdf85463ba5a59f2ebe
Lucas Aubard <lucas.aubard@algosup.com>
2024-02-12 16:07:21 +0100

    Update mnemonics.h
    Add the JMP and the CMP and the JE

--- Headers/mnemonics.h
+++ Headers/mnemonics.h
@@ -25,41 +25,73 @@ uint16_t CALL(int address);
 uint16_t RET();
 
 
-uint16_t MOV(uint16_t memoryLocation, uint16_t registerIndex)
+uint16_t MOV(uint16_t registerIndex, uint16_t memoryLocation)
 {
-   return memoryLocation = registerIndex;
+    registers[registerIndex] = memory[memoryLocation];
+    return 0;
+}
+
+uint16_t CALL(int address)
+{
+    // Save the current state
+    stack[++sp] = bp;          // base pointer
+    stack[++sp] = ip;          // return address
+    bp = sp;
+    ip = address;
+    return 0;
 }
 
 
 uint16_t CMP(int registerIndex1, int registerIndex2)
 {
-    if (registerIndex1 == registerIndex2)
+    if (registers[registerIndex1] == registers[registerIndex2])
     {
         flags = 1;
     }
     else
     {
-        flags = 0;
+        flags = 2;
     }
+    return 0;
+}
+
+uint16_t LOAD(int registerIndex, int memoryLocation)
+{
+    registers[registerIndex] = memory[memoryLocation];
+    return 0;
+}
+
+uint16_t STORE(int registerIndex, int memoryLocation)
+{
+    memory[memoryLocation] = registers[registerIndex];
     return 0;
 }
 
 
 
-uint16_t JE(uint16_t address)
+uint16_t JMP(int address)
+{
+    ip = address;
+    return 0;
+}
+
+uint16_t JE(int address)
 {
     if (flags == 1)
     {
-        return address;
+        ip = address;
     }
-    else
-    {
-        return 0;
-    }
+    return 0;
 }
 
 uint16_t RET()
 {
+    int return_value = stack[sp--];
+    sp = bp;                // deleting everything above the bp
+    ip = stack[sp--];
+    bp = stack[sp--];
+    sp -= stack[sp--];
+    stack[++sp] = return_value;
     return 0;
 }
 
