06c36ebdbc65d456abeb833312cae9a1058b21b0
Lucas Aubard <lucas.aubard@algosup.com>
2024-03-01 12:11:47 +0100

    optimisation of the compare instruction
    preparing a proper assembly code for the demonstration for the oral presentation.

--- src/arm16_vm.c
+++ src/arm16_vm.c
@@ -137,12 +137,15 @@ void execute_instruction(const char *instruction, int line) {
         STORE(dest_reg, op1_val);
         printf("  Stored value from %d to memory address in %d\n", dest_reg, op1_val);
     } else if (strcmp(op, "CMP:") == 0) {
+        sscanf(instruction, "    CMP: %s %s", op1, op2); // Parse instruction
     int result = arm_regs[reg_name_to_enum(op1)] - arm_regs[reg_name_to_enum(op2)];
     // Set flags based on the result
     if (result == 0) {
         flags = 1; // Set zero flag if result is zero
+        printf("    %s and %s are equal\n", op1, op2);
     } else {
         flags = 0; // Clear zero flag if result is not zero
+        printf("    %s and %s are not equal\n", op1, op2);
     }
     // No need to print the result for CMP
     } else if (strcmp(op, "JMP:") == 0) {
